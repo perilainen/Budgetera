133df2394c510f2317bd425a1ee940a04442210a
Per Johansson <perilainen@gmail.com>
2016-01-23 21:41:07 +0100

    Spara budget

---
+++ js/Budget.js
@@ -0,0 +1,30 @@
+var Budgets =[];
+function init(){
+	document.getElementById("saveBudget").addEventListener("click",saveBudgetLocal,false);
+	
+	console.log(localStorage.budgets);
+	var storedData = localStorage.getItem("budgets");
+	if (storedData===null){
+		console.log("Creating new budget")
+		Budgets = new Budget("Budget Name");
+	}
+	else{
+		Budgets = JSON.parse(localStorage.getItem("budgets"));
+		reDrawAll();
+
+		
+	}
+	
+	//Budgets = new Budget("Budget Name");
+}
+
+function saveBudgetLocal(){
+	localStorage.setItem("budgets",JSON.stringify(Budgets));
+}
+function Budget(name){
+	this.name = "name";
+	this.inkomster = [];
+	this.investeringar = [];
+	this.kostnader = []
+}
+window.addEventListener("load", init, false);
